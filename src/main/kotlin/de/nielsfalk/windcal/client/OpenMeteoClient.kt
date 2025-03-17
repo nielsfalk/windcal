5e4264a99ef1a296f4ae3b0b61db54aa5e2b7622
Niels Falk <niels@nielsfalk.de>
2025-03-17 21:48:02 +0100

    ktor client logging

--- src/main/kotlin/de/nielsfalk/windcal/client/OpenMeteoClient.kt
+++ src/main/kotlin/de/nielsfalk/windcal/client/OpenMeteoClient.kt
@@ -6,6 +6,7 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.ExperimentalSerializationApi
@@ -34,6 +35,10 @@ private suspend fun query(
                     namingStrategy = JsonNamingStrategy.SnakeCase
                 }
             )
+        }
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.BODY
         }
     }.use {
         it.get("https://api.open-meteo.com/v1/forecast") {
