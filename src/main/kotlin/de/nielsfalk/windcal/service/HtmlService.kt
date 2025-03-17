f85169220bf9b17746bb52c128575f5ef74f77ef
Niels Falk <niels@nielsfalk.de>
2025-03-17 17:26:24 +0100

    index.html use local storage, add initial data, add reset button

--- src/main/kotlin/de/nielsfalk/windcal/service/HtmlService.kt
+++ src/main/kotlin/de/nielsfalk/windcal/service/HtmlService.kt
@@ -1,5 +1,6 @@
 package de.nielsfalk.windcal.service
 
+import de.nielsfalk.windcal.domain.spots
 import io.ktor.server.plugins.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.Dispatchers
@@ -13,10 +14,32 @@ object HtmlService {
             object {}.javaClass.classLoader.getResource("index.html")!!
                 .readText()
                 .replace(
-                    oldValue = "!!baseurl!!",
+                    oldValue = "/*BASE_URL*/",
                     newValue = call.request.origin.run {
                         val port = serverPort.takeIf { it != 80 }
                         "https://$serverHost${port?.let { ":$it" } ?: ""}/wind.ics"
+                    }
+                )
+                .replace(
+                    oldValue = "/*INITIAL_DATA*/",
+                    newValue = spots.joinToString(separator = ",") {
+                        it.run {
+                            val windDirections = filter.windDirections?.joinToString(separator = "-")?:""
+                            //language=JSON
+                            """
+                            {
+                                "spotname": "$name",
+                                "latitude": "$latitude",
+                                "longitude": "$longitude",
+                                "minWindSpeed": "${filter.minWindSpeed}",
+                                "maxWindSpeed": "${filter.maxWindSpeed}",
+                                "maxGustSpeedOntop": "${filter.maxGustSpeedOntop}",
+                                "hours": "${filter.hoursOfMatchingConditions}",
+                                "windDirections": "$windDirections"
+                            }
+                            """.trimIndent()
+                        }
+
                     }
                 )
                 .also { indexHTML = it }
